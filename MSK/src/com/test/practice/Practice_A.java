8f72b6019daa2a7e64241695a525fe618102a22a
karth <karth@CR-053-Karthick>
2024-05-17 17:28:20 +0530

    1st commit test

---
+++ MSK/src/com/test/practice/Practice_A.java
@@ -0,0 +1,39 @@
+package com.test.practice;
+
+public abstract class Practice_A {
+
+	public static void method_A() {
+		byte a = 2;
+		short b = 4;
+		int c = 3;
+		
+		System.out.println("Method A sum = "+a+b+c);
+	}
+	public static long method_B() {
+		int x = 123;
+		short y = 456;
+		long z = 789;
+		
+		long t = x+y+z;
+		return t;
+	}
+	public static void method_C() {
+		int x = 123;
+		short y = 456;
+		long z = 789;
+		
+		long t = x+y+z;
+	}
+	public abstract void method_D();
+	
+	public static void main(String[] args) {
+		
+		method_A();
+		method_B();
+		method_C();
+		
+		System.out.println("Result of method_B 't' value = "+method_B());
+		System.out.println("Run successfully");
+	}
+
+}
---
+++ MSK/src/com/test/practice01/Practice_01.java
@@ -0,0 +1,35 @@
+package com.test.practice01;
+
+import com.test.practice.Practice_A;
+
+public class Practice_01 extends Practice_A {
+	
+	public static void method_01() {
+		long a = method_B();
+		System.out.println(a);
+	}
+	
+	public static void method_02(int s) {
+	
+		System.out.println(s);
+	}
+	public static void method_02(byte s) {
+		System.out.println(s);
+	}
+	@Override
+	public void method_D() {
+		System.out.println("Overrided method_D in child class");
+		
+	}
+	public static void main(String[] args) {
+	
+		method_01();
+		method_02(2123423427);
+		method_02(982342342);
+		Practice_01 pr = new Practice_01();
+		pr.method_D();
+	
+	}
+	
+
+}
