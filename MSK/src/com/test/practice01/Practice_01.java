2097fbe2f4e5dc660644efbf11246179d558ac4e
karth <karth@CR-053-Karthick>
2024-05-17 18:17:58 +0530

    3rd commit small changes in the main method

--- MSK/src/com/test/practice01/Practice_01.java
+++ MSK/src/com/test/practice01/Practice_01.java
@@ -29,11 +29,11 @@ public class Practice_01 extends Practice_A {
 	
 	public static void main(String[] args) {
 	
-//		method_01();
-//		method_02(2123423427);
+		method_01();
+		method_02(2123423427);
 		method_02(982342342);
-//		Practice_01 pr = new Practice_01();
-//		pr.method_D();
+		Practice_01 pr = new Practice_01();
+		pr.method_D();
 		pyramid();
 	
 	}
